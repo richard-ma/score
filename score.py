15ed65ba25b875c59c0d637f932168c3f6e54b04
Richard Ma <richard.ma.19850509@gmail.com>
2013-09-23 23:08:37 +0800

    处理成绩框架

--- score.py
+++ score.py
@@ -2,16 +2,51 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
+import traceback
+import logging
+import re
 
-dataDir = os.getcwd() + '/data'
-ansDir = os.getcwd() + '/ans'
+# 基础配置
+logging.basicConfig(level = logging.DEBUG)
 
-# 判断数据目录是否存在
-if not os.path.exists(dataDir):
-    print '没有data目录，请先创建数据'
+def getFileList(path):
+    path = str(path)
+    if path == "":
+      return []
+    a = os.listdir(path)
+    b = [x for x in a if os.path.isfile(path + x)]
+    return b
 
-# 判断输出目录是否存在
-if os.path.exists(ansDir):
-    print 'ans目录已存在，请删除此目录后重新执行。' # 已经存在输出目录，提示删除
-else:
-    os.makedirs(ansDir) # 创建输出目录
+if __name__ == '__main__':
+  try:
+    dataDir = os.getcwd() + '/data/'
+    ansDir = os.getcwd() + '/ans/'
+
+    # 判断数据目录是否存在
+    if not os.path.exists(dataDir):
+      logging.error('没有data目录，请先创建数据')
+      sys.exit(1)
+
+    '''
+    # 判断输出目录是否存在
+    if os.path.exists(ansDir):
+      logging.info('ans目录已存在，请删除或移动此目录后重新执行。')
+      sys.exit(1)
+    else:
+      os.makedirs(ansDir) # 创建输出目录
+      logging.info('已创建ans目录')
+    '''
+
+    for scoreFile in getFileList(dataDir): # 找出所有数据文件
+      logging.debug("**Opening " + dataDir + scoreFile)
+      with open(dataDir + scoreFile, 'r') as dataFile: # 打开数据文件
+        for line in dataFile: # 处理一个学生成绩
+          for score in line.split(','):
+            if re.match(r'[+-]?\d+$', score):
+              print score
+
+  except SystemExit:
+    pass
+  except:
+    traceback.print_exc()
